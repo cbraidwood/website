c540815431d00b01a514d12ea3dda483a4052f19
Charlie Braidwood <78386622+cbraidwood@users.noreply.github.com>
2022-07-27 20:38:50 +0000

    Change href for css.

--- build-site.el
+++ build-site.el
@@ -20,7 +20,7 @@
 (setq org-html-validation-link nil            ;; Don't show validation link
       org-html-head-include-scripts nil       ;; Use our own scripts
       org-html-head-include-default-style nil ;; Use our own styles
-	  org-html-head "<link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\" />")
+	  org-html-head "<link rel=\"stylesheet\" type=\"text/css\" href=\"https://raw.githubusercontent.com/cbraidwood/website/main/style.css\" />")
 
 	  
 ;; Define the publishing project
